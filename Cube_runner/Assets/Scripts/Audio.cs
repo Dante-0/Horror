be0ff1dba08135b9b2de6c0b176f6149ce4bd71b
Dante-0 <alisher.laiyk@gmail.com>
2020-10-07 18:00:02 +0600

    delete

--- Cube_runner/Assets/Scripts/Audio.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Audio:MonoBehaviour {
-  public void Music(AudioSource audio) {
-    audio.enabled = !audio.enabled;
-  }
-}
