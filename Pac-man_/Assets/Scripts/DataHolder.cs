64feda69d1c01b55ec09bb76be640d801bb8c980
Dante-0 <alisher.laiyk@gmail.com>
2020-10-27 11:55:48 +0600

    delete

--- Pac-man_/Assets/Scripts/DataHolder.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
-
-public class DataHolder:MonoBehaviour {
-  public static int Score { get; set; }
-}
